82958a153c5b9d4088e3a936ebd12e9b1e32a56a
Kevin GO <Kevin GO@DESKTOP-37TBAA5>
2023-05-07 02:20:52 -0500

    solucionado

---
+++ solucion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solucion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            double area;
+
+            Console.WriteLine("Ingrese el radio del círculo: ");
+            radio = double.Parse(Console.ReadLine());
+
+            area = Math.PI * Math.Pow(radio, 2);
+
+            Console.WriteLine("El área del círculo es: " + area);
+            Console.ReadLine();
+        }
+    }
+}
