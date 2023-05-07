c9c523f3dc3f4d9d0f87d330b69192ed7f54a40c
Kevin GO <Kevin GO@DESKTOP-37TBAA5>
2023-05-07 03:18:00 -0500

    nuevo

--- demostracion/Program.cs
+++ demostracion/Program.cs
@@ -10,6 +10,19 @@ namespace demostracion
     {
         static void Main(string[] args)
         {
+            int num1, num2, resultado;
+
+            Console.Write("Ingrese el primer número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = num1 - num2;
+
+            Console.WriteLine("La resta de {0} y {1} es: {2}", num1, num2, resultado);
+
+            Console.ReadKey();
         }
     }
 }
